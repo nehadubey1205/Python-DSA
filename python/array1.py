42eba02ee59860c93c7261ff8714ed28cbe50a24
nehadubey1205 <nehadubey1205@gmail.com>
2026-05-03 13:47:54 +0530

    2nd commmit

--- python/array1.py
+++ python/array1.py
@@ -6,4 +6,9 @@ for i in arr:
     if i>maxs:
         maximum = i
 print("max:",maximum)
-    
+## 2nd largest element
+second_max = arr[0]
+for i in arr:
+    if i>second_max and i<maximum:
+        second_max = i
+print("2nd max:",second_max)
