6958bc101263c0c4d28cd69ae157bbcb09041c18
nehadubey1205 <nehadubey1205@gmail.com>
2026-05-03 14:20:27 +0530

    reverse array user input

--- python/array2.py
+++ python/array2.py
@@ -1,4 +1,16 @@
-arr = [1,2,4,7,3]
+# arr = [1,2,4,7,3]
+
+# left = 0
+# right = len(arr) - left - 1
+# print(arr[left],arr[right])
+# while left<=right:
+#     arr[left],arr[right]= arr[right],arr[left]
+#     left = left+1
+#     right = right-1
+
+# print(arr)
+
+arr = list(map(int,input("Enter the array elements: ").split()))
 
 left = 0
 right = len(arr) - left - 1
@@ -9,5 +21,7 @@ while left<=right:
     right = right-1
 
 print(arr)
+
+
     
     
