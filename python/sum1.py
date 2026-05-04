f89211c48eea2f8ab24af4d7abd87bd59779b31e
nehadubey1205 <nehadubey1205@gmail.com>
2026-05-04 22:49:25 +0530

    user defined and sum of array

--- python/sum1.py
+++ python/sum1.py
@@ -7,3 +7,13 @@ while i <len(arr):
     sum= sum+arr[i]
     i=i+1
 print(sum)
+### user defined
+arr = list(map(int,input("enter the array element:").split()))
+sum=0
+i=0
+print(arr[i])
+while i <len(arr):
+
+    sum= sum+arr[i]
+    i=i+1
+print(sum)
